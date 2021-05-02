3c2d43ccf241ff09844f3886663a83b21398d34b
Igor Noia Silva <igornoiasilva@gmail.com>
2021-05-02 20:02:29 -0300

    commit

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer>
+        <p>
+            <strong>Developed</strong> by <a href="https://github.com/AleNoia" target="_blank">Igor Noia
+                Silva</a>
+        </p>
+        <div class="is-flex">
+            <a href="https://github.com/AleNoia" target="_blank">
+                <button class="btn button-footer mr-3">
+                    <i class="fab fa-github mr-3 fa-lg"></i>
+                    <span>Github</span>
+                </button>
+            </a>
+            <a href="https://www.linkedin.com/in/igor-noia-619a0820b/" target="_blank">
+                <button class="btn button-footer">
+                    <i class="fab fa-linkedin mr-3 fa-lg"></i>
+                    <span>Linkedin</span>
+                </button>
+            </a>
+        </div>
+    </footer>
+</template>
+
+
+
+<script>
+
+
+
+</script>
+
+
+<style src="./style.scss" lang="scss" scoped />
---
+++ src/components/Note/Note.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="content content-cards">
+        <div class="card">
+            <div class="card-content didnot">
+                <div>
+                    <h2 class="title-note">Lorem ipsum dolor sit amet.</h2>
+                </div>
+                <div class="card-footer">
+                    <h3 class="status"><i class="fas fa-times"></i>Incomplete</h3>
+                    <p>Created on 20/10/2021</p>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-content did">
+                <h2 class="title-note">Lorem ipsum dolor sit amet.</h2>
+                <div class="card-footer">
+                    <h3 class="status"><i class="fas fa-check"></i>Concluded</h3>
+                    <p>Concluded on 20/10/2021</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+
+
+
+</script>
+
+
+<style src="./style.scss" lang="scss" scoped />
